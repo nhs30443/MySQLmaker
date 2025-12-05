3f634fb90a714370b4c393957224a06d0dec817e
害 <0nj388387871k1p@ezweb.ne.jp>
2025-12-05 10:57:52 +0900

    maketable

---
+++ static/js/common.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.toggle-option').forEach(option => {
+    option.addEventListener('click', () => {
+        option.parentElement.querySelectorAll('.toggle-option').forEach(o => o.classList.remove('active'));
+        option.classList.add('active');
+    });
+});
